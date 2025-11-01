51811cbd712cd26481d3ddfda14324b5c7f07a8e
Aasiya Ekram <ekramaasiya98@gmail.com>
2025-11-01 14:13:28 +0530

    Files Modified

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-<script>
-    </script>
+
+      const cards = document.querySelector(".container");
+
+      const data = fetch("https://api.coinpaprika.com/v1/tickers/")
+        .then((res) => res.json())
+        .then((data) => data);
+      console.log(data);
+      data.then((data) => {
+        data.splice(0, 14).forEach((coin) => {
+          const card = document.createElement("div");
+          card.className = "card";
+          card.innerHTML = `
+            <h2>${coin.name} (${coin.symbol})</h2>
+            <p>Price: $${coin.quotes.USD.price.toFixed(2)}</p>
+            <a href=${`https://en.wikipedia.org/wiki/${coin.name}`} target="_blank"><button>Know history</button></a>
+        `;
+          cards.appendChild(card);
+        });
+      });
+    
